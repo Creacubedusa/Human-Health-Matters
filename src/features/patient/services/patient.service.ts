62dd9245ed2c2a515b98ab5d80fb13744fc9c9d0
Aphatheology <aphatheology@gmail.com>
2026-04-30 18:48:31 +0100

    fix

--- src/features/patient/services/patient.service.ts
+++
@@ -1,7 +0,0 @@
-import type { PatientHomeDashboard } from '../types/patient.types';
-import { http } from '@shared/api/http';
-
-export async function fetchPatientDashboard(): Promise<PatientHomeDashboard> {
-  const res = await http.get<PatientHomeDashboard>('/patients/dashboard');
-  return res.data;
-}
